22a9a9d966c264d6b0c302fb6e14bd4464fb9f8d
Bill Parrott <bill@chimericdream.com>
2016-08-26 00:17:50 -0500

    basic configuration for an adapter

--- src/helpers/hbs-helpers.js
+++ src/helpers/hbs-helpers.js
@@ -5,17 +5,15 @@ const hbs = require('hbs');
 hbs.registerHelper('equal', function(a, b, opts) {
     if (a === b) {
         return opts.fn(this);
-    } else {
-        return opts.inverse(this);
     }
+    return opts.inverse(this);
 });
 
 hbs.registerHelper('nequal', function(a, b, opts) {
     if (a === b) {
         return opts.inverse(this);
-    } else {
-        return opts.fn(this);
     }
+    return opts.fn(this);
 });
 
 hbs.registerHelper('sum', function(a, b) {
@@ -27,4 +25,11 @@ hbs.registerHelper('sum', function(a, b) {
 
 hbs.registerHelper('json', function(object) {
     return JSON.stringify(object, null, 4);
+});
+
+hbs.registerHelper('contains', function(array, value, opts) {
+    if (array.includes(value)) {
+        return opts.fn(this);
+    }
+    return opts.inverse(this);
 });
--- src/web/views/admin/configuration/index.js
+++ src/web/views/admin/configuration/index.js
@@ -1,14 +1,16 @@
 'use strict';
 
+const bodyParser = require('body-parser');
 const express = require('express');
+const fs = require('fs');
 const render = require('../../../../helpers/render');
 
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-const adapters = require('../../../../../config/adapters.json');
-
 router.get('/', (request, response) => {
+    const adapters = JSON.parse(fs.readFileSync('config/adapters.json'));
+
     render('admin/configuration/index.hbs', response, {
         'adapters': adapters
     });
@@ -16,7 +18,8 @@ router.get('/', (request, response) => {
 
 router.get('/:adapter*', (request, response) => {
     const configPath = request.path.replace(/^\/(.+)\/$/, '$1') + '.json';
-    const adapterConfig = require(`../../../../../config/adapters/${configPath}`);
+    const adapterConfig = JSON.parse(fs.readFileSync(`config/adapters/${configPath}`));
+    const adapters = JSON.parse(fs.readFileSync('config/adapters.json'));
 
     let adapter;
 
@@ -32,5 +35,40 @@ router.get('/:adapter*', (request, response) => {
         'config': adapterConfig
     });
 });
+
+router.post(
+    '/*',
+    bodyParser.urlencoded({ 'extended': false }),
+    (request, response) => {
+        if (!request.body) {
+            return response.sendStatus(400);
+        }
+
+        const configPath = request.path.replace(/^\/(.+)\/$/, '$1') + '.json';
+        const adapterConfig = JSON.parse(fs.readFileSync(`config/adapters/${configPath}`));
+        const adapters = JSON.parse(fs.readFileSync('config/adapters.json'));
+
+        let adapter;
+
+        adapter = {};
+        adapters.forEach((item) => {
+            if (item['config-file-path'] === configPath) {
+                adapter = item;
+            }
+        });
+
+        if (request.body.scopes) {
+            adapterConfig.scopes.value = [].concat(request.body.scopes);
+        }
+        adapterConfig['client-id'].value = request.body['client-id'];
+        adapterConfig['client-secret'].value = request.body['client-secret'];
+        fs.writeFileSync(`config/adapters/${configPath}`, JSON.stringify(adapterConfig, null, 4));
+
+        render('admin/configuration/adapter/index.hbs', response, {
+            'adapter': adapter,
+            'config': adapterConfig
+        });
+    }
+);
 
 module.exports = router;
